427cae261de6b8c3616a5ec5678489c626503608
Eric Leuthardt <7thcirclegit@users.noreply.github.com>
2023-12-12 13:22:20 -0600

    load site

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sarada's Remedies</title>
+	<style>
+		body { text-align: center; padding: 2em; }
+		img#index { display: block; margin: auto; max-width: 100%; }
+	</style>
+</head>
+<body>
+<p>
+	<img id="index" src="index-website-homepage-101423.png" alt="Sarada-Anasstasia Von Son, CAD, RH, BCTMB, RYT, Certificed Ayurvedic Doctor, Registered Herbalist. Experience: The healing therapies of the ancient art and science of Ayurveda. Sarada's Remedie's, Inc.; Anastasia's Remedies, LLC." />
+</p>
+<p>
+	<a href="index-website-homepage-101423.pdf">PDF version of this page</a>
+</p>
+<p>
+	A new website is in development.<br />
+	In the meantime, Visit Anastasia “Sarada” Von Sonn, Certified Ayurvedic Practitioner at:<br />
+	<strong><a href="https://www.soulbodyojai.com/practitioners/anastasia-sarada-von-sonn/">Soul Body Ojai</a></strong>.
+</p>
+</body>
+</html>
